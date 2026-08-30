d6e80a9c2f73fe9ee0b081aa4993feecf7ae0c70
Burak <buraksystems1@gmail.com>
2026-08-30 20:31:48 +0300

    Modul isim uymsuzlugunu ayarlardan tamamen duzelt

--- BurakArsivProvider/src/main/kotlin/recloudstream/InternetArchiveProvider.kt
+++ BurakArsivProvider/src/main/kotlin/recloudstream/InternetArchiveProvider.kt
@@ -13,7 +13,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.SubtitleFile
 
-class InternetArchiveProvider : MainAPI() { // Sınıf adını klasör ismiyle eşitledik
+class InternetArchiveProvider : MainAPI() {
     override var mainUrl = "https://example.com"
     override var name = "Burak Arşiv (Popüler Platformlar)"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
